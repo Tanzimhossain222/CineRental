6ed40c5ebff93df41a7689021aa4e0dcffe3c5ed
Tanzim Hossain <tanzimhossain2@gmail.com>
2024-01-26 22:08:32 +0600

    refactor: Update Rating component
    This commit includes changes to the Rating component. It includes:
    - Refactored the way stars are displayed
    - Stars are now dynamically generated based on the 'value' prop

--- src/components/cine/MovieCard.jsx
+++ src/components/cine/MovieCard.jsx
@@ -14,8 +14,7 @@ const MovieCard = ({ movie }) => {
           <h3 className="text-xl mb-1">{movie.title}</h3>
           <p className="text-[#575A6E] text-sm mb-2"> {movie.genre} </p>
           <div className="flex items-center space-x-1 mb-5">
-            {movie.rating > 0 &&
-              [...Array(movie.rating)].map((_, i) => <Rating key={i} />)}
+            <Rating value={movie.rating} />
           </div>
           <a
             className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
